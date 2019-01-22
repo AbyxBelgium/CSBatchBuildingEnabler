50d9b66dce3c0e53426cbd832b11d4afb395fbb2
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-01-22 17:10:15 +0100

    First steps of the mod

---
+++ CSBatchBuildingEnabler/CSBatchBuildingEnabler.cs
@@ -0,0 +1,19 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace CSBatchBuildingEnabler {
+    public class CSBatchBuildingEnabler: IUserMod {
+        public string Name {
+            get {
+                return "Batch Building Enabler";
+            }
+        }
+
+        public string Description
+        {
+            get {
+                return "Batch enable/disable all buildings of a specific type.";
+            }
+        }
+    }
+}
