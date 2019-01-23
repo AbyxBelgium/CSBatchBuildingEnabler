7acbb3bdb3ded8a026a86b539472a5838456ff63
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-01-23 20:40:25 +0100

    Fixed on/off button not updating

---
+++ CSBatchBuildingEnabler/CSBatchBuildingEnablerPanelMonitor.cs
@@ -0,0 +1,39 @@
+﻿using ColossalFramework;
+using ColossalFramework.UI;
+using ICities;
+using UnityEngine;
+
+namespace CSBatchBuildingEnabler
+{
+    public class CSBatchBuildingEnablerPanelMonitor: ThreadingExtensionBase {
+        private ShelterWorldInfoPanel panel;
+        private UICheckBox onOffCheckBox;
+       
+
+        public override void OnUpdate(float realTimeDelta, float simulationTimeDelta) {
+            Debug.Log("ON UPDATE!");
+            if (!FindComponents()) {
+                return;
+            }
+
+            if (panel.component.isVisible) {
+                ushort buildingId = ShelterWorldInfoPanel.GetCurrentInstanceID().Building;
+                
+                // display the right checkbox state  
+                onOffCheckBox.isChecked = Singleton<BuildingManager>.instance.m_buildings.m_buffer[buildingId].m_productionRate != 0; 
+                Debug.Log("IsChecked: " + onOffCheckBox.isChecked);
+            }
+        }
+
+        public bool FindComponents() {
+            if(panel != null && onOffCheckBox != null) return true;
+
+            panel = UIView.library.Get<ShelterWorldInfoPanel>(typeof(ShelterWorldInfoPanel).Name);
+            if (panel == null) return false;
+            
+           
+            onOffCheckBox = panel.component.Find<UICheckBox>("On/Off");
+            return onOffCheckBox != null;
+        }
+    }
+}
